a4d4a6febb0edcf9b41e1fd5d9f562f10e3c8626
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2023-09-10 18:40:13 +0500

    :construction: Removed extra comment file

--- src/components/extraCommentCode.jsx
+++
@@ -1,8 +0,0 @@
-      {/* <SyntaxHighlighter language={children?.language} style={coldarkDark}
-        wrapLines={true}
-        customStyle={{
-          borderRadius: "10px"
-        }}
-      >
-        {children?.code}
-      </SyntaxHighlighter> */}
