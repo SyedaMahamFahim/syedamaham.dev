e463b97a9f28ebe1a98a4c2b6e973d907ffd0e66
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2023-09-14 03:28:19 +0500

    :lipstick: Fixed style issues at opensource pagination

--- src/app/(content)/open-source/page.tsx
+++ src/app/(content)/open-source/page.tsx
@@ -1,10 +1,10 @@
 import { Text, ContentsTypeTab } from "@/components";
-import { OpenSourceCard, Loader } from "@/components/";
 import {WEBSITE_NAME} from '@/constants/_APP_SETUP'
 import { Metadata } from "next";
 import { SanityDocument } from "@sanity/client";
 import { getOpenSourceQuery } from "@/sanity/lib/queries";
 import { sanityFetch } from "@/sanity/lib/sanityFetch";
+import {OpenSourceSection} from "@/containers"
 
 export const metadata: Metadata = {
   title:'Open Source Projects',
@@ -30,21 +30,10 @@ const OpenSource = async () => {
           >
             Open Source 🔓 
           </Text>
-          <div className="grid grid-cols-1 xs:grid-cols-1 sm:grid-cols-2 gap-3 xs:gap-5 md:gap-6 pb-4 lg:pb-8 md:grid-cols-3 lg:grid-cols-4">
-            {openSourceProjects.length === 0 ? (
-              <Loader />
-            ) : (
-              openSourceProjects?.map((openSource: any, index: number) => {
-                return (
-                  <OpenSourceCard
-                    key={index}
-                    url={openSource.url}
-                    title={openSource.title}
-                  />
-                );
-              })
-            )}
-          </div>
+          <OpenSourceSection
+          OS={openSourceProjects}
+          noOfOS={20}
+          />
         </div>
       </section>
     </>
