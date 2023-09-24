0c3359548f88da87f774e2d9a8d02fe821c654fd
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2023-09-24 06:05:28 +0500

    fixed env for deploy button

--- sanity/env.ts
+++ sanity/env.ts
@@ -10,7 +10,7 @@ export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
-export const previewToken= assertValue(process.env.NEXT_PUBLIC_SANITY_READ_TOKEN,'Missing environment variable:NEXT_PUBLIC_SANITY_READ_TOKEN')
+export const previewToken= assertValue(process.env.SANITY_API_READ_TOKEN,'Missing environment variable:SANITY_API_READ_TOKEN')
 
 export const useCdn = false
 
